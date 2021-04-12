30c94b014ffb585568eed99332c0d58efcc192d8
Youssef Khaled <youlof123@gmail.com>
2021-04-12 19:09:39 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from tkinter import ttk
+import tkinter as tk
+from ttkthemes import ThemedTk
+
+GREY = "#dddddd"
+FONT_DATA = ("TkMenuFont", 10, "normal")
+window = ThemedTk(theme="arc", background=True)
+window.title("Speedest")
+window.config(padx=50, pady=50)
