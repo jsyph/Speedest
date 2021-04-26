64d6948ad23a875f580d9337d16aec5461bc7032
Youssef Khaled <youlof123@gmail.com>
2021-04-26 12:49:56 +0200

    Finished version 0.9

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ REC_HEIGHT = 200
 RECTANGLE_COLOUR = "#3fc1c9"
 RECTANGLE_TEXT_COLOUR = "#364f6b"
 BACKGROUND_COLOUR = "#f5f5f5"
+RED = "#fc5185"
+YELLOW = "#ffde7d"
 WIDGET_WIDTH = 95
 font_data = ("TkMenuFont", 20, "normal")
 window = ThemedTk(theme="adapta")
@@ -30,18 +32,43 @@ def show_time(event):
 def start_test():
     controlls_frame.grid_remove()
     test_text_canvas.itemconfig(test_text, text="Starting....")
-    test_entery_label.grid(row=1, column=0, pady=(20, 0), padx=(0, 20))
-    test_entery.grid(row=2, column=0)
+    test_frame.grid(row=1, column=0, pady=(20, 0))
+    test_entery.focus()
 
     brain.get_scentence()
     test_text_canvas.itemconfig(test_text, text=brain.scentence)
+    print(math.ceil(float(scale_variable.get())))
+    brain.difficulty_time = math.ceil(float(scale_variable.get()))
+    brain.start_timer()
 
 
-def change_text(event: tk.Event):
+def change_text(event):
+    brain.check_timer()
     if event.keysym == "space":
         brain.get_scentence()
         test_text_canvas.itemconfig(test_text, text=brain.scentence)
-        brain.index += 1
+        if brain.is_test_complete["state"]:
+            window.focus_set()
+            test_entery.grid_remove()
+            show_result_btn.grid(row=2, column=0)
+            brain.end_timer()
+            test_entery_label.config(text=brain.is_test_complete["message"])
+            test_text_canvas.itemconfig(test_text, text=brain.is_test_complete["message"])
+        else:
+            brain.index += 1
+            brain.words_left -= 1
+            test_entery_label.config(text=f"{str(brain.words_left)} words left")
+
+
+def show_result():
+    window.focus_set()
+    brain.check_answer(str(test_entery.get()))
+    test_frame.grid_remove()
+    test_text_canvas.grid_remove()
+    test_result_frame.grid(row=0, column=0)
+    title_canvas.itemconfig(title_text, text=brain.is_test_complete["message"])
+    errors_canvas.itemconfig(errors_text, text=f"{brain.user_score['errors']} wrong words.")
+    speed_canvas.itemconfig(speed_text, text=f"{brain.user_score['typing_speed']} words/second")
 
 
 # ----------------------------------------------------------------------------------------------------------------------
@@ -79,8 +106,57 @@ start_button.grid(row=2, column=0, pady=(20, 0))
 controlls_frame.grid(row=1, column=0, pady=(20, 0))
 
 # ----------------------------------------------------------------------------------------------------------------------
-test_entery_label = ttk.Label(window, text="Start typing the words above.", font=(font_data[0], 10, font_data[2]))
-test_entery = ttk.Entry(window, width=WIDGET_WIDTH)
+test_frame = ttk.Frame(window)
+test_entery_label = ttk.Label(test_frame, text="Start typing the words above.", font=(font_data[0], 10, font_data[2]))
+test_entery = ttk.Entry(test_frame, width=WIDGET_WIDTH)
 test_entery.bind("<KeyPress>", change_text)
+show_result_btn = ttk.Button(test_frame, width=WIDGET_WIDTH, text="Show results", command=show_result)
+
+test_entery_label.grid(row=0, column=0)
+test_entery.grid(row=1, column=0)
+# ----------------------------------------------------------------------------------------------------------------------
+result_rec_height = REC_HEIGHT
+result_rec_width = REC_WIDTH
+small_rec_width = result_rec_width / 2
+font = "Futura 20 bold"
+test_result_frame = ttk.Frame(window)
+
+# Title canvas that displays the brain.is_test_complete["message"]
+title_canvas = tk.Canvas(test_result_frame, width=result_rec_width, height=result_rec_height,
+                         background=BACKGROUND_COLOUR,
+                         highlightthickness=0)
+title_rec = round_rectangle(title_canvas, 0, 0, result_rec_width, result_rec_height, radius=50,
+                            fill=RECTANGLE_COLOUR,
+                            width=result_rec_width)
+title_text = title_canvas.create_text(result_rec_width / 2, result_rec_height / 2,
+                                      fill=RECTANGLE_TEXT_COLOUR,
+                                      font=font)
+
+# Errors canvas that displays the brain.user_score["errors"]
+errors_canvas = tk.Canvas(test_result_frame, width=small_rec_width, height=result_rec_height,
+                          background=BACKGROUND_COLOUR,
+                          highlightthickness=0)
+errors_rec = round_rectangle(errors_canvas, 0, 0, small_rec_width, result_rec_height, radius=50,
+                             fill=RED,
+                             width=small_rec_width)
+errors_text = errors_canvas.create_text(small_rec_width / 2, result_rec_height / 2,
+                                        fill=RECTANGLE_TEXT_COLOUR,
+                                        font=font)
+
+# Speed canvas which displayes the brain.user_score["typing_speed"]
+speed_canvas = tk.Canvas(test_result_frame, width=small_rec_width, height=result_rec_height,
+                         background=BACKGROUND_COLOUR,
+                         highlightthickness=0)
+speed_rec = round_rectangle(speed_canvas, 0, 0, small_rec_width, result_rec_height, radius=50,
+                            fill=YELLOW,
+                            width=small_rec_width)
+speed_text = speed_canvas.create_text(small_rec_width / 2, result_rec_height / 2,
+                                      fill=RECTANGLE_TEXT_COLOUR,
+                                      font=font)
+
+title_canvas.grid(row=0, column=0, columnspan=2)
+errors_canvas.grid(row=1, column=0)
+speed_canvas.grid(row=1, column=1)
+
 # ----------------------------------------------------------------------------------------------------------------------
 window.mainloop()
--- testbrain.py
+++ testbrain.py
@@ -1,6 +1,7 @@
 import json
 import random
 import datetime as dt
+import math
 
 
 class TestBrain:
@@ -9,13 +10,30 @@ class TestBrain:
             self.paragraph_data = json.load(file)
         self.chosen_text = str(random.choice(self.paragraph_data)["text"])
         self.splitted_text = self.chosen_text.split(" ")
-        self.start_time = None
-        self.stop_time = None
+        self.words_left = len(self.splitted_text)
+        self.total_words = len(self.splitted_text)
+        self.start_time = 0
+        self.stop_time = 0
+        self.time_taken = 0
         self.index = 0
+        self.difficulty_time = 0
         self.scentence = None
+        self.is_test_complete = {"state": False, "message": ""}
+        self.user_score = {
+            "errors": 0,
+            "typing_speed": 0
+        }
+
+    def refresh_paragraph_data(self):
+        with open("paragraphs.json", encoding="utf8") as file:
+            self.paragraph_data = json.load(file)
+        self.chosen_text = str(random.choice(self.paragraph_data)["text"])
+        self.splitted_text = self.chosen_text.split(" ")
+        self.words_left = len(self.splitted_text)
 
     def get_scentence(self):
         if self.index == 0:
+            word0 = " "
             word1 = " "
             word2 = self.splitted_text[0]
             word3 = self.splitted_text[1]
@@ -25,21 +43,73 @@ class TestBrain:
             word7 = self.splitted_text[5]
             word8 = self.splitted_text[6]
         else:
-            word1 = self.splitted_text[self.index-1]
-            word2 = self.splitted_text[self.index]
-            word3 = self.splitted_text[self.index+1]
-            word4 = self.splitted_text[self.index+2]
-            word5 = self.splitted_text[self.index+3]
-            word6 = self.splitted_text[self.index+4]
-            word7 = self.splitted_text[self.index+5]
-            word8 = self.splitted_text[self.index+6]
-        self.scentence = f"{word1} {word2} {word3} {word4} {word5} {word6} {word7} {word8}"
+            word0 = self.splitted_text[self.index - 2]
 
-    def check_answer(self, test_result):
-        pass
+            word1 = self.splitted_text[self.index - 1]
+
+            try:
+                word2 = self.splitted_text[self.index]
+            except IndexError:
+                word0 = " "
+                word1 = " "
+                word2 = " "
+                self.is_test_complete["state"] = True
+                self.is_test_complete["message"] = "Test is complete, 0 words left."
+
+            try:
+                word3 = self.splitted_text[self.index + 1]
+            except IndexError:
+                word3 = " "
+
+            try:
+                word4 = self.splitted_text[self.index + 2]
+            except IndexError:
+                word4 = " "
+
+            try:
+                word5 = self.splitted_text[self.index + 3]
+            except IndexError:
+                word5 = " "
+
+            try:
+                word6 = self.splitted_text[self.index + 4]
+            except IndexError:
+                word6 = " "
+
+            try:
+                word7 = self.splitted_text[self.index + 5]
+            except IndexError:
+                word7 = " "
+
+            try:
+                word8 = self.splitted_text[self.index + 6]
+            except IndexError:
+                word8 = " "
+
+        self.scentence = f"{word0} {word1} {word2} {word3} {word4} {word5} {word6} {word7} {word8}"
+
+    def check_answer(self, test_result: str):
+        self.check_timer()
+        result_list = test_result.split(" ")
+        wrong_answers = 0
+        for right, wrong in zip(self.splitted_text, result_list):
+            if right != wrong:
+                wrong_answers += 1
+        self.user_score["errors"] = wrong_answers
+        self.user_score["typing_speed"] = math.ceil(len(result_list) / self.time_taken)
+        print(self.user_score)
 
     def start_timer(self):
-        pass
+        self.start_time = dt.datetime.now().second
+
+    def end_timer(self):
+        self.stop_time = dt.datetime.now().second
 
     def check_timer(self):
-        pass
+        self.end_timer()
+        self.time_taken = self.stop_time - self.start_time
+        print(self.time_taken)
+        if self.time_taken >= self.difficulty_time:
+            print("Fuuuuuuuuuuuuuuuuuuuuuuuuuuuuck")
+            self.is_test_complete["state"] = True
+            self.is_test_complete["message"] = "Time Over!"
